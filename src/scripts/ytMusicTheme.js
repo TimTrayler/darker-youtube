a8d398c0c6e0ac9184c213d0393670d0c897436c
TimTrayler <67274738+TimTrayler@users.noreply.github.com>
2021-12-26 20:02:16 +0100

    YTM Lightmode Improvements

--- src/scripts/ytMusicTheme.js
+++ src/scripts/ytMusicTheme.js
@@ -1,4 +1,4 @@
 
-/*if(window.matchMedia("(prefers-color-scheme: light)").matches ||true) {
+/*if(window.matchMedia("(prefers-color-scheme: light)").matches ||!0) {
     document.querySelector("html").setAttribute("dark", false)
 }*/
